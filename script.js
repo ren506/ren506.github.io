4e8e14be66d37cb22f26f0e16d5dda9793d12ab2
ren506 <1725650109@qq.com>
2025-03-16 00:49:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,8 @@ noButton.addEventListener("click", function () {
   if (clickCount === 2) mainImage.src = "images/think.png"; // 思考
   if (clickCount === 3) mainImage.src = "images/angry.png"; // 生气
   if (clickCount === 4) mainImage.src = "images/crying.png"; // 哭
-  if (clickCount >= 5) mainImage.src = "images/crying.png"; // 之后一直是哭
+  if (clickCount === 5) mainImage.src = "images/"QQ.jpg;// 嚎啕大哭
+  if (clickCount >= 6) mainImage.src = "images/crying.png"; // 之后一直是哭
 });
 
 // Yes 按钮点击后，进入表白成功页面

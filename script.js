debddf9f2be75973832f0e066bbd7da1e51a6a61
ren506 <1725650109@qq.com>
2025-03-16 00:51:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ noButton.addEventListener("click", function () {
 });
 
 // Yes 按钮点击后，进入表白成功页面
-const loveTest = `!!!喜欢你!! ( >᎑<)♡︎ᐝ  ${
+const loveTest = `!!!超级喜欢你!! ( >᎑<)♡︎ᐝ  ${
   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
 }`;
 
